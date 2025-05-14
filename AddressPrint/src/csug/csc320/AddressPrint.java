a22a35e3399add64d214a03a9a2ce1cf7ef6d996
Jeremy Gant <JeremyTheGant@gmail.com>
2025-05-14 17:07:47 -0600

    Initial commit AddressPrint for Mod1b.

---
+++ AddressPrint/src/csug/csc320/AddressPrint.java
@@ -0,0 +1,19 @@
+package csug.csc320;
+
+public class AddressPrint {
+
+	public static void main(String[] args) {
+		String nameFirst = "Jeremy";
+		String nameLast = "Gant";
+		String streetAddress = "631 Carlsbad Dr";
+		String city = "Grand Junction";
+		String zipCode = "81507";		//zip code not treated as number, therefore string
+		
+		System.out.println(nameFirst);
+		System.out.println(nameLast);
+		System.out.println(streetAddress);
+		System.out.println(city);
+		System.out.println(zipCode);
+	}
+
+}
