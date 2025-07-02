f8480253496fb2fc10af7dd2242dab9bba35e011
Jeremy Gant <JeremyTheGant@gmail.com>
2025-07-01 21:43:44 -0600

    Adding try, throw, catch

--- AddressPrint/src/csug/csc320/AddressPrint.java
+++ AddressPrint/src/csug/csc320/AddressPrint.java
@@ -1,43 +1,49 @@
 package csug.csc320;
+
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class AddressPrint {
 
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
-		//ArrayList<Address> addressList = new ArrayList<>();
-		
+
 		System.out.println("Please enter your name and address in the following format or ");
-		System.out.println("Type \"q\" to quit.\n");
+		System.out.println("Type \"q\" to quit.");
 		System.out.println();
 		System.out.println("first name, last name, address, city, state, zip code");
-		
+
 		String input = scnr.nextLine();
-		
+
 		while (!input.equalsIgnoreCase("q")) {
-			String [] inputParts = input.trim().split("\\s*,\\s*");
-			if (inputParts.length != 6) {
-				System.out.println("Invalid format. Please enter exactly 6 comma-separated values\n");
+			try {
+				String[] inputParts = input.trim().split("\\s*,\\s*");
+
+				if (inputParts.length != 6) {
+					throw new IllegalArgumentException(
+							"Invalid format. Please enter exactly 6 comma-separated values.");
+				}
+
+				Address myAddress = new Address(inputParts[0], inputParts[1], inputParts[2], inputParts[3],
+						inputParts[4], inputParts[5]);
+
+				System.out.println();
+				myAddress.printAddress();
+
+				input = "q";
+			}
+
+			catch (IllegalArgumentException e) {
+				System.out.println(e.getMessage());
+				System.out.println();
 				input = scnr.nextLine();
 				continue;
 			}
-			Address myAddress = new Address(
-				inputParts[0], inputParts[1], inputParts[2],
-				inputParts[3], inputParts[4], inputParts[5]);
-			
-			System.out.println();
-			myAddress.printAddress();
-			
-			input = "q";
-			continue;
 		}
-		
-		if (input.equalsIgnoreCase("q")) {
-			System.out.println("Goodbye");
-			scnr.close();
-			System.exit(0);
-		}
-		
+
+			if (input.equalsIgnoreCase("q")) {
+				System.out.println("Goodbye");
+				scnr.close();
+				System.exit(0);
+			}
 	}
 }
